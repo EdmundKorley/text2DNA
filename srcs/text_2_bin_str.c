e1075ac972eeb7aa4f2dac5834bda28890e3fd62
Edmund Korley <korley.em@gmail.com>
2016-09-06 20:22:13 -0400

    Make sure rows are even numbered for our triplet conversion step

--- srcs/text_2_bin_str.c
+++ srcs/text_2_bin_str.c
@@ -28,7 +28,7 @@ char	**text_to_bin_str(char *str)
 }
 
 /*
-** This helper turns a given ASCII value
+** This function turns a given ASCII value
 ** into a string of bits.
 */
 char	*ascii_to_bin(int ascii)
@@ -42,7 +42,7 @@ char	*ascii_to_bin(int ascii)
 	bin = 0;
 	i = 1;
 	j = 0;
-	bit_str = malloc(ASCII_BITS_MAXLEN);
+	bit_str = malloc(ASCII_BITS_MAXLEN + 2); // This needs to be an even # for the triplet conversion (sans \0).
 	while (ascii)
 	{
 		leftover = ascii % 2;
@@ -54,6 +54,6 @@ char	*ascii_to_bin(int ascii)
 	}
 	bit_str[j] = '\0';
 	ft_revstr(bit_str);
-	bit_str = ft_leftpad(bit_str, '0', ASCII_BITS_MAXLEN);
+	bit_str = ft_leftpad(bit_str, '0', ASCII_BITS_MAXLEN + 1);
 	return (bit_str);
 }
