f44399799a892c04d7edb944163a00abc73dd2bf
Hannah Lily Postman <postman.hl@gmail.com>
2016-09-07 11:49:39 -0700

    Keep extended ASCII in mind, and define 'BASE' macro for semantics and flexibility

--- includes/text2dna.h
+++ includes/text2dna.h
@@ -6,7 +6,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define ASCII_BASE7_MAXLEN 4 // E.G. 127 is 1333 (4 digits)
+#define ASCII_BASE7_MAXLEN 4 // E.G. 255 (max extended ASCII value) is 3333 (4 digits)
+#define BASE 4 // If we revert to base 2 in the future, we only have to change the code here
 
 char	**text_to_base4_str(char *str);
 void	ft_revstr(char *str);
